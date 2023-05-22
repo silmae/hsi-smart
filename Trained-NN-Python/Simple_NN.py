1583446ac986639ec1af848982343dff8bd71865
JJ <jajoutzs@jyu.fi>
2023-05-22 10:40:26 +0300

    Update Simple_NN.py

--- Trained-NN-Python/Simple_NN.py
+++ Trained-NN-Python/Simple_NN.py
@@ -2,34 +2,75 @@ import tensorflow as tf
 import numpy as np
 import os
 import csv
-import tensorflow.keras.utils
-from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import TensorBoard, Callback
+from PIL import Image
+
 # This file contains NN implementation for trained NN, i.e program assumes calculated weights 
 # Created:       05.05.2023
-# Last modified: 12.05.2023
+# Last modified: 22.05.2023
 # @ Jukka J
 
 # Defining callback function for TensorBoard (to save logs)
-log_dir = "./logs/"
-tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+# log_dir = "./logs/"
+# tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
 
+output_dir_test_data = "./Test-Data"
+
+#  Class to store the layer outputs during training in the layer_outputs attribute.
+class SaveLayerOutputsCallback(Callback):
+    def __init__(self, model, num_images, validation_data):
+        super(SaveLayerOutputsCallback, self).__init__()
+        self.model = model
+        self.num_images = num_images
+        self.validation_data = validation_data
+        self.layer_outputs = []
+
+    def on_epoch_end(self, epoch, logs=None):
+        output_func = tf.keras.backend.function([self.model.input], [layer.output for layer in self.model.layers])
+        outputs = output_func(self.validation_data[0][:self.num_images])
+        outputs_np = [np.array(output) for output in outputs]  # Convert TensorFlow tensors to NumPy arrays
+        self.layer_outputs.append(outputs_np)
+
+        # Save output values to CSV files:
+  
+        output_dir = "./Tensorflow-Layer-Outputs"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        for layer_index, layer_output in enumerate(outputs_np):
+            layer_name = f"layer_{layer_index + 1}"
+            for image_index, image_output in enumerate(layer_output):
+                image_name = f"image_{image_index + 1}"
+                output_filename = f"{layer_name}_{image_name}_output.csv"
+                output_filepath = os.path.join(output_dir, output_filename)
+                np.savetxt(output_filepath, image_output, delimiter=",")
+                print(f"Output values for {layer_name}, {image_name} saved to {output_filepath} in CSV format.")
+                
+                if layer_index == 0:
+                    output_filename = f"mnist_img_{image_index}.csv"
+                    output_filepath_test_data = os.path.join(output_dir_test_data, output_filename)
+                    np.savetxt(output_filepath_test_data, image_output, delimiter=",")
 
 # Loading of MNIST dataset
-# 60k images in the training set and 10k images in the test set. 
-# mnist = tf.keras.datasets.mnist
-os.chdir('/home/jukka/HSI-Smart/archive')
+# 60k images in the training set and 10k images in the test set.
+
+# Change directory to "archive"
+os.chdir(os.path.join(os.getcwd(), '..', 'archive'))
 
 # np.genfromtxt() reads in the header line of the CSV file as well, which adds an extra row to the resulting arrays.
 # Therefore header should be skipped
 mnist_train =  np.genfromtxt('mnist_train.csv', delimiter=',', skip_header=1)
 mnist_test =  np.genfromtxt('mnist_test.csv', delimiter=',', skip_header=1)
 
-os.chdir('/home/jukka/HSI-Smart/Trained-NN-Python')
+
+# Change directory back to "Trained-NN-Python"
+os.chdir(os.path.join(os.getcwd(), '..'))
+
 # Data splitting
 # Split the datasets into input features and labels:
 
-# 1 Select all rows in the array and all columns except the first column. 
-# 2 select all rows in the array and only the first 
+# 1 Select all rows in the array and all columns except the first column.
+# 2 select all rows in the array and only the first
 x_train, y_train = mnist_train[:, 1:], mnist_train[:, 0]
 x_test, y_test = mnist_test[:, 1:], mnist_test[:, 0]
 
@@ -80,47 +121,59 @@ ml_model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(10, activation='softmax')   # Output layer with 10 neurons, one for each class in the MNIST dataset. This layer has 32*10+10 = 330 parameters to learn.
 ])
 
-
-# Vector of logits 
-predictions = ml_model(x_train[:1]).numpy()
-
-# Logits to probabilities
-#tf.nn.softmax(predictions).numpy()
-
-# Loss function for training
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
-# The loss function takes a vector of ground truth values and a vector of logits and returns a scalar loss for each example
-# (and output to numpy array)
-loss_fn(y_train[:1], predictions).numpy()
-
 # Configuring and compiling the model:
 # Class for optimization: adam
 # Loss function to be used: own
 # Metrics to be evaluated, accuracy
  
 ml_model.compile(optimizer='adam',
-              loss=loss_fn,
+              loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-              
+
 ml_model.summary()
 
-# Iterations of entire dataset set to 5
-ml_model.fit(x_train, y_train, epochs=5, callbacks=[tensorboard_callback])
+
+# Change directory to "Cpp-Implementation"
+os.chdir(os.path.join(os.getcwd(), 'Cpp-Implementation'))
+current_folder = os.getcwd()
+print("Current folder:", current_folder)
+
+layer_output_callback = SaveLayerOutputsCallback(ml_model, num_images=10, validation_data=(x_test, y_test))
+
+# Iterations of entire dataset set to 15
+ml_model.fit(x_train, y_train, epochs=15, validation_data=(x_test, y_test), callbacks=[layer_output_callback])
+
+
+# Get the output of each layer for the first 10 images in the test set
+#output_values = [layer_output_callback.layer_outputs[layer_index][0][:10] for layer_index in range(len(layer_output_callback.layer_outputs))]
+# Save output values to CSV files
+
+output_dir = "./Tensorflow-Output-Values"
+
+# Create the output directory if it does not exist
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 #======================Saving weights and biases of each layer in .csv format ===============================
 print(ml_model.layers)
 
 # Get the weights of the model
 weights = ml_model.get_weights()
-print(weights)
+# Loop through the layers and print the weights and biases
+for i, (w, b) in enumerate(zip(weights[::2], weights[1::2])):
+    print(f"Layer {i + 1}:")
+    print("Weights:")
+    print(w)
+    print("Biases:")
+    print(b)
+    print()
+
 
 name_array_weights = ["1st_hidden_weights.csv", "2nd_hidden_weights.csv", "3rd_hidden_weights.csv", "output_layer_weights.csv"]
 name_array_biases = ["1st_hidden_bias.csv", "2nd_hidden_bias.csv", "3rd_hidden_bias.csv", "output_layer_bias.csv"]
 name_array_folders = ["1st-Hidden", "2nd-Hidden", "3rd-Hidden", "Output-Layer"]
-output_dir = "../Cpp-Implementation"
+output_dir = os.getcwd()
 
-print()
 # Loop through the layers and save the weights and biases in separate csv files
 for i, (w, b) in enumerate(zip(weights[::2], weights[1::2])):
     folder_path = os.path.join(output_dir, name_array_folders[i])
@@ -130,33 +183,7 @@ for i, (w, b) in enumerate(zip(weights[::2], weights[1::2])):
     weights_file_path = os.path.join(folder_path, name_array_weights[i])
     biases_file_path = os.path.join(folder_path, name_array_biases[i])
     np.savetxt(weights_file_path, w, delimiter=",")
-    print(f"  Weights shape: {w.shape}")
+    print(f"Weights shape: {w.shape}")
     np.savetxt(biases_file_path, b, delimiter=",")
-    print(f"  Biases shape: {b.shape}")
-
-
+    print(f"Biases shape: {b.shape}")
 print("Weights and biases saved successfully!")
-
-# Get the output of the last layer for the first 10 images in the test set
-output = ml_model.layers[-1].output
-output_func = tf.keras.backend.function([ml_model.input], [output])
-output_values = output_func(x_test[:10])[0]
-
-# Save output values to csv files
-# Set precision of 10 numbers after decimal
-# Define the output directory
-output_dir = "../Cpp-Implementation/Tensorflow-Output-Values"
-
-# Create the output directory if it does not exist
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-
-# Loop through the output data and save it to separate csv files
-for i, output_data in enumerate(output_values):
-    output_filepath = os.path.join(output_dir, f"output_{i}.csv")
-    if output_data.ndim == 1:
-        np.savetxt(output_filepath, output_data.reshape(1, -1), delimiter=",", fmt="%.15f")
-    else:
-        np.savetxt(output_filepath, output_data, delimiter=",", fmt="%.15f,"*(output_data.shape[1]-1) + "%.15f")
-    print(f"Output values for image {i} saved to {output_filepath} in csv format")
-
