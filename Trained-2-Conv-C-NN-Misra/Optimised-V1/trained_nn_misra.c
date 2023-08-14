228a039e7560e4ae3b0889b9c6501d995a2a4512
JJ <jajoutzs@jyu.fi>
2023-08-14 21:36:22 +0300

    Update trained_nn_misra.c

--- Trained-2-Conv-C-NN-Misra/Optimised-V1/trained_nn_misra.c
+++ Trained-2-Conv-C-NN-Misra/Optimised-V1/trained_nn_misra.c
@@ -1,6 +1,7 @@
 #include <hdf5.h>
-#include "datparser.h"
+#include "dat_parser.h"
 #include "tools.h"
+#include <stdlib.h>
 
 /* Created:       25.05.2023
    Last modified: 11.08.2023
@@ -14,45 +15,56 @@ int main() {
     /* ---------- Loading data for testing start------------------------------------ */
 
     LineData data[1];
-    int numLines = 0;
+    int num_lines = 0;
     const char* filename = "../archive/mineral-spectra_denoised_norm_spectra.dat";
 
-    parseFile(filename, data, &numLines);
+    parse_file(filename, data, &num_lines);
     
     /* ---------- Loading data for testing end -------------------------------------- */
 
-    /* Looping through test data, and passing data through NN */
+    /* Looping through (1st image only in this case) test data, and passing data through NN */
 
     for (int i = 0; i < 1; i++) {
 
         /* Single image as input, and corresponding size */
         double * input = data[i].values;
         int i_s = data[i].numValues;
-        
+
         /*========== Function calling sequence: ==========================*/
         
         /* --------- First normalization layer --------- */
-        Batch_normalization_1(input, i_s);
+        batch_normalization_1(input, i_s);
 
         /* --------- First convolution layer ----------- */
         double ** arr1 = convolution_layer_1(input, i_s);
-            
+        
         /* --------- Second normalization layer -------- */
-        Batch_normalization_2_and_3(arr1, i_s, 2);
+        batch_normalization_2_and_3(arr1, i_s, 2);
 
         /* --------- Second convolution layer ---------- */
         double ** arr2 = convolution_layer_2(arr1, i_s);
+        /* Freeing allocated memory */
+        int padding_start_and_end = (KS - 1) / 2;
+        free_2D_array(arr1, (i_s + padding_start_and_end * 2));
         
         /* --------- Third normalization layer --------- */
-        Batch_normalization_2_and_3(arr2, i_s, 3);
+        batch_normalization_2_and_3(arr2, i_s, 3);
         
         /* --------- Flatten layer --------------------- */
         double* arr3 = flatten_layer(arr2, i_s);
+        /* Freeing allocated memory */
+        free_2D_array(arr2, i_s);
         
         /* --------- Dense layer ----------------------- */
         double* arr4 = dense_layer(arr3, DLWD1_AND_DLIC);
+        /* Freeing allocated memory */
+        free(arr3);
         
+        /* TODO: do something with produced predictions (for single image) */
 
+        /* Freeing allocated memory */
+        free(arr4);
+        
         /* ================================================================ */
     }
     
